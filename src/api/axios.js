15869e195171f7031c68bd02c09ae61ee26e63f2
Julian Rojas <juliandrojasa140201@gmail.com>
2026-09-08 13:08:04 -0500

    feature: Login (fix)

---
+++ src/api/axios.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+const api = axios.create({
+    baseURL: VITE_API_URL, headers: {
+        'Content-Type': 'application/json',
+    }
+});
+// Opcional: Interceptor para enviar el token JWT automáticamente si existe
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+export default api;
